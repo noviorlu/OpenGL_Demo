989a3dc6b4308d35bd05ae8c59d9bb6b17ed8096
Yang Chen <jackchen.chen@mail.utoronto.ca>
2024-03-23 10:14:39 -0400

    init OpenGL proj

---
+++ src/Application.cpp
@@ -0,0 +1,66 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+        std::cout << "Glew Initalization Error!" << std::endl;
+
+    /* Print OpenGL version */
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+
+    unsigned int buffer; /* PointerORindex to generated buffer */
+    glGenBuffers(1, &buffer);
+    /* Define propose of the buffer */
+    glBindBuffer(GL_ARRAY_BUFFER, buffer); 
+    /* Define buffer Size, pass in preprocessed data */
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);   
+    /* Specify how OpenGL interperts the data (3 vertices, float, 2 float represent 1 vertex, etc.) */
+    /* Generally: How our Data is Layed out?*/
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Draw Triangle */
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
